3138b24f60b5f6a59cc3c2e3379ba1999c41b020
MOKLET-1 <MOKLET-1@LAPTOP-UT3A7DI1>
2020-12-07 16:34:36 -0800

    remidiujian

---
+++ src/remidi/ujian/RemidiUjian.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remidi.ujian;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class RemidiUjian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
